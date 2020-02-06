5c7faaa6753f1d579da802047895800fbb8e51b4
Begüm Üner <begumuner@hotmail.com>
2020-02-06 15:41:39 +0300

    no message

--- pro/Program.cs
+++ pro/Program.cs
@@ -10,20 +10,40 @@ namespace pro
         static void Main(string[] args)
         {
 
-            string b = pdfText(Directory.GetCurrentDirectory()+ "/Net.pdf");
+            string[] arr = new string[] {"Müşterimiz","Değerli","fjdujhf"};
+            var newArr = Add(arr, "UYARI");
 
-            string a= "kullan";
-            int sonuc;
-            sonuc = b.IndexOf(a,0, b.Length);
-            if(sonuc==-1)
+            string b = pdfText(Directory.GetCurrentDirectory() + "/v.pdf");
+
+            for (int i = 0; i < newArr.Length; i++)
             {
-                Console.WriteLine("not found");
+                string a = newArr[i];
+                int sonuc;
+                sonuc = b.IndexOf(a, 0, b.Length);
+                if (sonuc == -1)
+                {
+                    Console.WriteLine("not found");
+                }
+                else
+                {
+                    Console.WriteLine("found from " + sonuc.ToString() + ". character");
+                }
             }
-            else
-            {
-                Console.WriteLine("found from " + sonuc.ToString() + ". character");
-            } 
 
+        }
+
+        public static string[] Add(string[] array, string newValue)
+        {
+            int newLength = array.Length + 1;
+
+            string[] result = new string[newLength];
+
+            for (int i = 0; i < array.Length; i++)
+                result[i] = array[i];
+
+            result[newLength - 1] = newValue;
+
+            return result;
         }
 
         public static string pdfText(string path)
@@ -35,10 +55,11 @@ namespace pro
             {
                 System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
                 text += PdfTextExtractor.GetTextFromPage(reader, page);
-                
+
             }
             reader.Close();
             return text;
         }
+        
     }
 }
